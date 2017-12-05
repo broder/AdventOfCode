1bc826999f8be9edbd271dad0d4c18f294564e38
David Broder-Rodgers <broder93@gmail.com>
2017-12-05 06:58:35 +0000

    Add solution for 2017 Day 5

---
+++ AdventOfCode/2017/Day05.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode._2017
+{
+    internal class Day05 : BaseDay
+    {
+        public override void RunPartOne()
+        {
+            Console.WriteLine(GetSteps(new[] {0, 3, 0, 1, -3}));
+            Console.WriteLine(GetSteps(LoadInput().Select(int.Parse).ToArray()));
+        }
+
+        private static int GetSteps(int[] instructions, bool extraStrangeInstructions = false)
+        {
+            var steps = 0;
+            var currentPosition = 0;
+            
+            while (currentPosition >= 0 && currentPosition < instructions.Length)
+            {
+                var nextPosision = currentPosition + instructions[currentPosition];
+                
+                if (extraStrangeInstructions && instructions[currentPosition] >= 3)
+                    instructions[currentPosition]--;
+                else
+                    instructions[currentPosition]++;
+                
+                currentPosition = nextPosision;
+                steps++;
+            }
+            
+            return steps;
+        }
+
+        public override void RunPartTwo()
+        {
+            Console.WriteLine(GetSteps(new[] {0, 3, 0, 1, -3}, true));
+            Console.WriteLine(GetSteps(LoadInput().Select(int.Parse).ToArray(), true));
+        }
+    }
+}
