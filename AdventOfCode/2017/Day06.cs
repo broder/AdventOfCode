04fd073dbbb24de8fb10fa75b88ebed0eabec1ea
David Broder-Rodgers <broder93@gmail.com>
2017-12-06 07:36:41 +0000

    Add solution for 2017 Day 6

---
+++ AdventOfCode/2017/Day06.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2017
+{
+    internal class Day06 : BaseDay
+    {
+        public override void RunPartOne()
+        {
+            Console.WriteLine(CalculateReallocation(new[] {0, 2, 7, 0}));
+            Console.WriteLine(CalculateReallocation(LoadInput().First().Split('	').Select(int.Parse).ToArray()));
+        }
+
+        private static int CalculateReallocation(int[] blocks, bool returnLoopSize = false)
+        {
+            var seenStates = new Dictionary<string, int>();
+            var currentState = string.Join(":", blocks.Select(i => i.ToString()));
+
+            while (!seenStates.ContainsKey(currentState))
+            {
+                seenStates.Add(currentState, seenStates.Count);
+
+                var maxValue = -1;
+                var maxIndex = -1;
+                for (var i = 0; i < blocks.Length; i++)
+                {
+                    if (blocks[i] <= maxValue) continue;
+                    maxIndex = i;
+                    maxValue = blocks[i];
+                }
+                blocks[maxIndex] = 0;
+
+                var currentIndex = Mod(maxIndex + 1, blocks.Length);
+                while (maxValue > 0)
+                {
+                    blocks[currentIndex]++;
+                    maxValue--;
+                    currentIndex = Mod(currentIndex + 1, blocks.Length);
+                }
+
+                currentState = string.Join(":", blocks.Select(i => i.ToString()));
+            }
+
+            if (returnLoopSize)
+                return seenStates.Count - seenStates[currentState];
+            return seenStates.Count;
+        }
+
+        public override void RunPartTwo()
+        {
+            Console.WriteLine(CalculateReallocation(new[] {2, 4, 1, 2}, true));
+            Console.WriteLine(CalculateReallocation(LoadInput().First().Split('	').Select(int.Parse).ToArray(), true));
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -4,7 +4,7 @@
     {
         private static void Main()
         {
-            new _2017.Day05();
+            new _2017.Day06();
         }
     }
 }
