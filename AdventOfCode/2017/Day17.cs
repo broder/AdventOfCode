685c1540530ea047f5ef74e534f4f0a9cc966122
David Broder-Rodgers <broder93@gmail.com>
2017-12-17 08:13:52 +0000

    Add solution for 2017 Day 17

---
+++ AdventOfCode/2017/Day17.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode._2017
+{
+    internal class Day17 : BaseDay
+    {
+        protected override void RunPartOne()
+        {
+            Console.WriteLine(GetSubsequentValue(9, 3));
+            Console.WriteLine(GetSubsequentValue(2017, 3));
+            Console.WriteLine(GetSubsequentValue(2017, 349));
+        }
+
+        private static int GetSubsequentValue(int size, int step)
+        {
+            var list = new LinkedList<int>(new int[1]);
+            var current = list.First;
+
+            for (var i = 1; i <= size; i++)
+            {
+                for (var j = 0; j < step; j++)
+                    current = GetNextNode(current);
+
+                var next = new LinkedListNode<int>(i);
+                list.AddAfter(current, next);
+                current = next;
+            }
+            return GetNextNode(current).Value;
+        }
+
+        private static LinkedListNode<int> GetNextNode(LinkedListNode<int> node) => node.Next ?? node.List.First;
+
+        protected override void RunPartTwo()
+        {
+            Console.WriteLine(GetSubsequentZeroValue(50000000, 349));
+        }
+
+        private static int GetSubsequentZeroValue(int size, int step)
+        {
+            var currentIndex = 0;
+            var value = 0;
+            for (var i = 1; i <= size; i++)
+            {
+                currentIndex = Mod(currentIndex + step, i) + 1;
+                if (currentIndex == 1)
+                    value = i;
+            }
+            return value;
+        }
+    }
+}
