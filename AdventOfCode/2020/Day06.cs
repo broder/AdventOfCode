294aa4cd4686032103fc7db7bc918eb8050786ec
David Broder-Rodgers <broder93@gmail.com>
2020-12-06 10:29:08 +0100

    Add solution for 2020 Day 6

---
+++ AdventOfCode/2020/Day06.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2020
+{
+    internal class Day06 : BaseDay
+    {
+        protected override void RunPartOne()
+        {
+            Console.WriteLine(GetQuestionGroups(LoadInput("practice")).Sum(g => g.Item2.Count()));
+            Console.WriteLine(GetQuestionGroups(LoadInput()).Sum(g => g.Item2.Count()));
+        }
+
+        private static IEnumerable<Tuple<int, Dictionary<char, int>>> GetQuestionGroups(string[] questions)
+        {
+            var people = 0;
+            var currentQuestions = new Dictionary<char, int>();
+            foreach (var line in questions) {
+                if (line.Length == 0) {
+                    yield return new Tuple<int, Dictionary<char, int>>(people, currentQuestions);
+                    people = 0;
+                    currentQuestions = new Dictionary<char, int>();
+                    continue;
+                }
+
+                people++;
+                foreach (var c in line) {
+                    if (!currentQuestions.ContainsKey(c))
+                        currentQuestions[c] = 0;
+                    currentQuestions[c]++;
+                }
+            }
+
+            if (people > 0)
+                yield return new Tuple<int, Dictionary<char, int>>(people, currentQuestions);
+        }
+
+        protected override void RunPartTwo()
+        {
+            Console.WriteLine(GetQuestionGroups(LoadInput("practice")).Sum(g => g.Item2.Count(c => c.Value == g.Item1)));
+            Console.WriteLine(GetQuestionGroups(LoadInput()).Sum(g => g.Item2.Count(c => c.Value == g.Item1)));
+        }
+    }
+}
